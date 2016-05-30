43b9c84f5f49b1e07d8da39d5b2faa99a7e07dba
MasakiyoShimoda <hi14shimoda@gmail.com>
2016-05-30 11:26:39 +0900

    Update 1-1.c

--- 1-1.c
+++ 1-1.c
@@ -8,35 +8,27 @@
 
 #include <stdio.h>
 
-double setProduct(productNumber){
-    int i, productValue[20] = {0};
-    double total;
-    for (i = 0; i < productNumber; i++) {
-        printf("商品%d:", i + 1);
-        scanf("%d", &productValue[i]);
-        total += productValue[i];
-    }
-    printf("計(税別):%lf", total);
-    return total;
-}
-
-
-int addTax(tax, totalProductValue){
-    tax = tax * 0.01 + 1.0;
-    totalProductValue *= tax;
-    printf("計(税別):%d", totalProductValue);
-    return totalProductValue;
-}
+#define TAX 1.08;
+#define PRODUCT_NUMVER 3;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    int productNumber = 3;
-    double total;
-    double tax = 8.0;
-    total = setProduct(productNumber);
-    addTax(tax, total);
+    int i, total = 0, productNum = PRODUCT_NUMVER;
+    double tax = TAX;
+    int productValue[20] = {0};
+    for (i = 0; i < productNum; i++) {
+        printf("商品%d:", i+1);
+        scanf("%d", &productValue[i]);
+        for (; productValue[i] > 100000 || productValue[i] < 0; ) {
+            printf("正しい価格を入力してください\n");
+            printf("商品%d:", i+1);
+            scanf("%d", &productValue[i]);
+        }
+        total += productValue[i];
+    }
+    printf("計（税別）:%d\n", total);
+    printf("税率: %.lf％\n", tax * 100 - 100);
+    printf("計（税込）:%.lf\n", (double)total * tax);
     
     return 0;
 }
-
-
