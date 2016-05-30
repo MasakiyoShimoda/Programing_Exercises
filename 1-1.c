ce74c4d5125bd554d48576d3a59b5a72c4f6ecf6
MasakiyoShimoda <hi14shimoda@gmail.com>
2016-05-30 10:54:30 +0900

    Update 1-1.c

--- 1-1.c
+++ 1-1.c
@@ -8,28 +8,15 @@
 
 #include <stdio.h>
 
-double setProduct(int);
-int addTax(double, double);
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    int productNumber = 3, i;
-    double tax = 8.0;
-    i = setProduct(productNumber);
-    addTax(tax, i);
-    
-    return 0;
-}
-
 double setProduct(productNumber){
-    int i, productValue[20] = {0}, 
+    int i, productValue[20] = {0};
     double total;
     for (i = 0; i < productNumber; i++) {
         printf("商品%d:", i + 1);
         scanf("%d", &productValue[i]);
         total += productValue[i];
     }
-    printf("計(税別):%.d", total);
+    printf("計(税別):%lf", total);
     return total;
 }
 
@@ -37,6 +24,19 @@ double setProduct(productNumber){
 int addTax(tax, totalProductValue){
     tax = tax * 0.01 + 1.0;
     totalProductValue *= tax;
-    printf("計(税別):%lf", totalProductValue);
+    printf("計(税別):%d", totalProductValue);
     return totalProductValue;
 }
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int productNumber = 3;
+    double total;
+    double tax = 8.0;
+    total = setProduct(productNumber);
+    addTax(tax, total);
+    
+    return 0;
+}
+
+
