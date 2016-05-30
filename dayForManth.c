892d41aa9c0a3d06b00f0f0d23cdfc7d5d1defd1
MasakiyoShimoda <hi14shimoda@gmail.com>
2016-05-30 11:47:45 +0900

    Create dayForManth.c

---
+++ dayForManth.c
@@ -0,0 +1,45 @@
+//
+//  main.c
+//  daysForManth
+//
+//  Created by 下田将斉 on 2016/05/30.
+//  Copyright © 2016年 Masakiyo Shimoda. All rights reserved.
+//
+
+#include <stdio.h>
+#include <assert.h>
+
+int daysForManth(int);
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    assert(daysForManth(0) == 0);
+    assert(daysForManth(1) == 31);
+    assert(daysForManth(2) == 28);
+    assert(daysForManth(5) == 31);
+    assert(daysForManth(9) == 30);
+    assert(daysForManth(11) == 30);
+    assert(daysForManth(12) == 31);
+    assert(daysForManth(13) == 0);
+    printf("All Passed\n");
+    
+    return 0;
+}
+
+int daysForManth(month){
+    int shortManth [] = {4,6,9,11};
+    int i, days = 31;
+    if(month == 0 || month > 12)
+        days = 0;
+    
+    if (month == 2)
+        days = 28;
+    
+    for (i = 0; i < 4; i++) {
+        if (month == shortManth[i]) {
+            days = 30;
+            break;
+        }
+    }
+    return days;
+}
