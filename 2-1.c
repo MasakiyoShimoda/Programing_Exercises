d71a1629ecf390a1ff3eed9f2bd97ef7f7a5e951
MasakiyoShimoda <hi14shimoda@gmail.com>
2016-07-04 11:38:04 +0900

    Update 2-1.c

--- 2-1.c
+++ 2-1.c
@@ -10,25 +10,26 @@
 #include <time.h>
 
 void printtime(long totalsec){
-  int sec, min, hour, day;
-  sec = totalsec % 60;
-  min = totalsec / 60;
-  hour = totalsec / (60 * 60);
-  day = totalsec / (24 * 60 * 60);
-  printf("%d\n",sec);
-
+    int sec, min, hour, day;
+    day = totalsec / (24 * 60 * 60);
+    hour = totalsec / (60 * 60) - (day * 24);
+    min = totalsec  / (60) - (day * 24 * 60) - (hour * 60);
+    sec = totalsec % 60;
+    
+    printf("%dd %02d:%02d:%02d\n",day, hour, min, sec);
+    
 }
 
 int main(int argc, const char * argv[]) {
-  puts("7265秒は:");
-  printtime(7265);
-
-  puts("200000は:");
-  printtime(200000);
-
-  puts("1970年1月1日から現在までの経過時間は:");
-  time_t current;
-  current = time(NULL);
-  printtime((long)current);
+    puts("7265秒は:");
+    printtime(7265);
+    
+    puts("200000は:");
+    printtime(200000);
+    
+    puts("1970年1月1日から現在までの経過時間は:");
+    time_t current;
+    current = time(NULL);
+    printtime((long)current);
     return 0;
 }
