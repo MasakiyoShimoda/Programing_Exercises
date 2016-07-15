f37caac3f7ffb2d05ce8bce18061b8c0f35ef116
MasakiyoShimoda <hi14shimoda@gmail.com>
2016-07-15 14:40:50 +0900

    Update 2-3.c

--- Problem2/2-3.c
+++ Problem2/2-3.c
@@ -7,22 +7,32 @@
 //
 
 #include <stdio.h>
+#include <time.h>
 
 int dayofweek_zeller(int year, int month, int day);
 
 int main(int argc, const char * argv[]) {
     // insert code here...
+    struct tm timeinfo;
     int year = -1, month = -1, day = -1;
     char *dayname[] = {"日曜日","月曜日","火曜日","水曜日","木曜日","金曜日","土曜日"};
     
     while (year < 1582 || month > 12 || month < 1) {
         year = month = day = -1;
-        printf( "Input 年月日(yyyy mm dd）: " );
-        scanf("%d %d %d", &year, &month, &day);
+        printf( "Input 年月日(yyyy/mm/dd）: " );
+        scanf("%d/%d/%d", &year, &month, &day);
         printf("%d%d%d", year, month, day);
     }
+    timeinfo.tm_year = year - 1900;
+    timeinfo.tm_mon  = month - 1;
+    timeinfo.tm_mday = day;
+    timeinfo.tm_hour = 0;
+    timeinfo.tm_min  = 0;
+    timeinfo.tm_sec  = 0;
+    time_t ct = mktime(&timeinfo);
     
-    printf("%d年%d月%d日は%sです\n",year ,month, day, dayname[dayofweek_zeller(year, month, day)]);
+    printf("%d年%d月%d日は%sです(mktime)\n",year ,month, day,dayname[timeinfo.tm_mday]);
+    printf("%d年%d月%d日は%sです(zeller)\n",year ,month, day, dayname[dayofweek_zeller(year, month, day)]);
     return 0;
 }
 
