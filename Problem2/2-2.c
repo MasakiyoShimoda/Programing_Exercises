04c333e75c4c43ad3deef21ca8f2b5b7ba8775c9
MasakiyoShimoda <hi14shimoda@gmail.com>
2016-07-23 03:26:31 +0900

    Update 2-2.c

--- Problem2/2-2.c
+++ Problem2/2-2.c
@@ -6,31 +6,28 @@
 //  Copyright © 2016年 Masakiyo Shimoda. All rights reserved.
 //
 
-
 #include <stdio.h>
 #include <time.h>
 #include <limits.h>
 
-void printtime(time_t totalsec) {
-    char *wday_name[] = {"Sun","Mon", "Thu","Wed","Thu", "Fri", "Sat"};
+void printtime(time_t totalsec){
+    char *wday_name[] = { "Sun", "Mon", "Thu", "Wed", "Thu", "Fri", "Sat" };
     struct tm *timeinfo;
     timeinfo = gmtime(&totalsec);
     
-    if(totalsec > 0)
+    if (totalsec > 0)
         printf("%d-%02d-%02d(%s) %02d:%02d:%02d\n\n",
-               timeinfo -> tm_year + 1900,
-               timeinfo -> tm_mon + 1,
-               timeinfo -> tm_mday,
-               wday_name[timeinfo -> tm_wday],
-               timeinfo -> tm_hour,
-               timeinfo -> tm_min,
-               timeinfo -> tm_sec
+               timeinfo->tm_year + 1900,
+               timeinfo->tm_mon + 1,
+               timeinfo->tm_mday,
+               wday_name[timeinfo->tm_wday],
+               timeinfo->tm_hour,
+               timeinfo->tm_min,
+               timeinfo->tm_sec
                );
-    else
-        puts("ERROR");
 }
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[]){
     puts("7265秒は:");
     printtime(7265);
     
@@ -42,13 +39,13 @@ int main(int argc, const char * argv[]) {
     current = time(NULL);
     printtime(current);
     
-    /*符号付き32bit INT型が表示可能な最大値*/
-    printf("%d秒は:\n", INT_MAX);
+    // 符号付き32bitの表示可能な最大値
+    puts("intで表現できる最大値:");
     printtime(INT_MAX);
     
-    /*符号付き64bit　LONG型の整数が表示可能な最大値*/
-    printf("%ld秒は: 表示不可\n", LONG_MAX);
-    /*printtime(LONG_MAX);*/
+    // 符号付き64bitの表示可能な最大値
+    printf("longで表現できる最大値: 【表現不可】\n");
+    //printtime(LONG_MAX);
     
     return 0;
 }
