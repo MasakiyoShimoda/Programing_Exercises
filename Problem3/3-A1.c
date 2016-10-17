7eedc7e32c5347b9141971cc92712cdaba78a74f
MasakiyoShimoda <hi14shimoda@gmail.com>
2016-10-17 11:41:08 +0900

    Update 3-A1.c

--- Problem3/3-A1.c
+++ Problem3/3-A1.c
@@ -29,7 +29,7 @@ int main(int argc, const char * argv[]) {
     srandom((unsigned int)time(NULL));
     
     puts("Find the missing number!");
-    // count();
+    count();
     start = microtime();
     question(x, xlen);
     end = microtime();
